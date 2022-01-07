f1ac90a2f35249eac80733f268df8d29b8e6d816
Walter Grandinetti <wgrandinetti@gmail.com>
2022-01-07 20:37:09 -0300

    helper function

---
+++ test/hello_3_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+/*
+Testing with helper function
+*/
+func TestHello3(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello2("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello2("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
--- test/hello_test.go
+++ test/hello_test.go
@@ -3,15 +3,26 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	t.Helper()
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello2("Chris")
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello2("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
 
-func TestHello3(t *testing.T) {
+func TestHellob(t *testing.T) {
 	got := Hello2("Chris")
 	want := "Hello, TestChris"
 
